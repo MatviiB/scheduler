3578a2fa0e3e5e9baeeae8d0bb84726cf82162c9
alireza setayesh <alireza@alirezas-MacBook-Air.local>
2019-04-19 20:03:34 +0430

    updated routes and add middleware

--- src/config/scheduler.php
+++ src/config/scheduler.php
@@ -11,9 +11,11 @@ return [
     | url - url for service managing page
     |
     */
+
     'enabled' => env('SCHEDULER_ENABLED', false),
-    'table'   => env('SCHEDULER_TABLE', 'scheduler'),
-    'url'     => env('SCHEDULER_URL', 'scheduler'),
+    'table' => env('SCHEDULER_TABLE', 'scheduler'),
+    'url' => env('SCHEDULER_URL', 'scheduler'),
+    'middleware' => ['auth'], // separate middleware by comma
 
     'nav-button' => [
         'href' => '/',
@@ -21,16 +23,16 @@ return [
     ],
 
     'expressions' => [
-        '* * * * *'      => 'Every minute',
-        '*/5 * * * *'    => 'Every 5 minutes',
-        '*/10 * * * *'   => 'Every 10 minutes',
-        '*/15 * * * *'   => 'Every 15 minutes',
-        '*/30 * * * *'   => 'Every 30 minutes',
-        '0 * * * *'      => 'Every hour',
-        '0 0 * * *'      => 'One per day',
-        '0 0 * * 0'      => 'One per week',
-        '0 0 1 * *'      => 'One per month',
+        '* * * * *' => 'Every minute',
+        '*/5 * * * *' => 'Every 5 minutes',
+        '*/10 * * * *' => 'Every 10 minutes',
+        '*/15 * * * *' => 'Every 15 minutes',
+        '*/30 * * * *' => 'Every 30 minutes',
+        '0 * * * *' => 'Every hour',
+        '0 0 * * *' => 'One per day',
+        '0 0 * * 0' => 'One per week',
+        '0 0 1 * *' => 'One per month',
         '0 0 1 1-12/3 *' => 'One per 2 months',
-        '0 0 1 1 *'      => 'One per year',
+        '0 0 1 1 *' => 'One per year',
     ],
 ];
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,20 +1,23 @@
 <?php
 
-$url = config('scheduler.url');
-$controller = 'MatviiB\\Scheduler\\Controllers\\SchedulerController';
+Route::group(['middleware' => config('scheduler.middleware')], function () {
+    $url = config('scheduler.url');
+    $controller = 'MatviiB\\Scheduler\\Controllers\\SchedulerController';
+    //
+    Route::get($url, ['as' => "$url.index", 'uses' => "$controller@index"]);
 
-Route::get($url, "$controller@index")->name("$url.index");
+    Route::get("$url/edit/{task}", ['as' => "$url.edit", 'uses' => "$controller@edit"]);
 
-Route::get("$url/edit/{task}", "$controller@edit")->name("$url.edit");
+    Route::patch("$url/update/{task}", ['as' => "$url.update", 'uses' =>  "$controller@update"]);
 
-Route::patch("$url/update/{task}", "$controller@update")->name("$url.update");
+    Route::get("$url/toggle/{task}", ['as' => "$url.toggle", 'uses' => "$controller@toggle"]);
 
-Route::get("$url/toggle/{task}", "$controller@toggle")->name("$url.toggle");
+    Route::get("$url/run/{task}", ['as' => "$url.run", 'uses' => "$controller@run"]);
 
-Route::get("$url/run/{task}", "$controller@run")->name("$url.run");
+    Route::get("$url/create", ['as' => "$url.create", 'uses' => "$controller@create"]);
 
-Route::get("$url/create", "$controller@create")->name("$url.create");
+    Route::post("$url/store", ['as' => "$url.store", 'uses' => "$controller@store"]);
 
-Route::post("$url/store", "$controller@store")->name("$url.store");
+    Route::delete("$url/delete", ['as' => "$url.delete", 'uses' => "$controller@delete"]);
 
-Route::delete("$url/delete", "$controller@delete")->name("$url.delete");
+});
